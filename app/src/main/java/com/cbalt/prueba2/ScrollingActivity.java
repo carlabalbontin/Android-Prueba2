0e4973ed330d598e07a045a2227483daa74fb5d3
Charlie <carlabalbontin@hotmail.com>
2018-07-28 12:42:16 -0400

    Eliminando recursos no utilizados

--- app/src/main/java/com/cbalt/prueba2/ScrollingActivity.java
+++ app/src/main/java/com/cbalt/prueba2/ScrollingActivity.java
@@ -2,19 +2,13 @@ package com.cbalt.prueba2;
 
 import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
@@ -34,7 +28,7 @@ public class ScrollingActivity extends AppCompatActivity implements FriendClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         RecyclerView recyclerView = findViewById(R.id.friendsRecyclerView);
@@ -57,7 +51,7 @@ public class ScrollingActivity extends AppCompatActivity implements FriendClickL
         adapter = new FriendsAdapter(this);
         recyclerView.setAdapter(adapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
